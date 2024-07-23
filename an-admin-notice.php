8a8590cbe6c75ed5b6a29b4ef9a5bb89cc050060
odedstr <dodothedremer@gmail.com>
2024-07-23 22:26:51 +0300

    v1.3: Updated github hook, and deploy script

--- an-admin-notice.php
+++ an-admin-notice.php
@@ -3,7 +3,7 @@
 	Plugin Name: Admin Notice
 	Plugin URI: https://github.com/animals-now/an-admin-notice
 	Description: Displays a custom notice to all users in the admin area.
-	Version: 1.2
+	Version: 1.3
 	Requires at least: 5.4.1
 	Requires PHP: 7.0
 	Author: Adam Walter, Oded Streigold
